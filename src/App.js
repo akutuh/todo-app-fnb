3e6508b35980b3cfd7440a1dbbcd52d9d20e7417
akutuh <akutuh@uef.fi>
2022-08-18 16:11:39 +0300

    added getall addTodo and deleteTodo functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoForm from './components/TodoForm'
 import Todos from './components/Todos'
+import todoService from './services/todos'
 
 import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
 
-  const addTodo = (todoObject) => {
-    setTodos(todos.concat(todoObject))
+  useEffect(() => {
+    todoService.getAll().then((todos) => setTodos(todos))
+  }, [])
+
+  const addTodo = async (todoObject) => {
+    try {
+      const response = await todoService.create(todoObject)
+      setTodos(todos.concat(response))
+    } catch (exception) {
+      console.log(exception.message)
+    }
   }
 
-  const deleteTodo = (todoObject) => {
-    setTodos(todos.filter((todo) => todo.id !== todoObject.id))
+  const deleteTodo = async (todoObject) => {
+    try {
+      if (window.confirm(`Remove todo ${todoObject.content}?`)) {
+        await todoService.remove(todoObject)
+        setTodos(todos.filter((todo) => todo.id !== todoObject.id))
+      }
+    } catch (exception) {
+      console.log(exception.message)
+    }
   }
 
   return (
     <div className="App">
+      <h2>Today's todo's</h2>
       <TodoForm createTodo={addTodo} />
       <Todos todos={todos} deleteTodo={deleteTodo} />
     </div>
