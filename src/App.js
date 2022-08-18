a35cb04e646f0b445859f2d287d0f4569904cb88
akutuh <akutuh@uef.fi>
2022-08-18 15:54:48 +0300

    basic read todos and create todo functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+import TodoForm from './components/TodoForm'
+import Todos from './components/Todos'
+
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todoObject) => {
+    setTodos(todos.concat(todoObject))
+  }
+
+  return (
+    <div className="App">
+      <TodoForm createTodo={addTodo} />
+      <Todos todos={todos} />
+    </div>
+  )
 }
 
 export default App
